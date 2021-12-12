34643a7585fdf3a38cc623d6bb6d76eb4140b95b
PhucSmRePo <0306191060@caothang.edu.vn>
2021-12-12 21:02:21 +0700

    update hinhanh diadanh

--- lib/dia_danh_object.dart
+++ lib/dia_danh_object.dart
@@ -2,25 +2,21 @@ class DiaDanhObject {
   final int id;
   final String tenDiaDanh;
   final String moTa;
-  final String noiDung;
-  final String hinhAnh;
   final String kinhDo;
   final String viDo;
   final int soLuongChiaSe;
-  final int idVung;
-  final int idMien;
+  final int soLuongLuotXem;
+  final int idTinhThanh;
   final int trangThai;
 
   DiaDanhObject(
       this.id,
       this.tenDiaDanh,
       this.moTa,
-      this.noiDung,
-      this.hinhAnh,
       this.kinhDo,
       this.viDo,
       this.soLuongChiaSe,
-      this.idVung,
-      this.idMien,
+      this.soLuongLuotXem,
+      this.idTinhThanh,
       this.trangThai);
 }
---
+++ lib/hinh_anh_object.dart
@@ -0,0 +1,22 @@
+class HinhAnhObject {
+  final int idHinhAnh;
+  final int idQuanAn;
+  final int idMonAn;
+  final int idLuuTru;
+  final int idDiaDanh;
+  final int idBaiVietChiaSe;
+  final int idLoai;
+  final String hinhAnh;
+  final int trangThai;
+
+  HinhAnhObject(
+      this.idHinhAnh,
+      this.idQuanAn,
+      this.idMonAn,
+      this.idLuuTru,
+      this.idDiaDanh,
+      this.idBaiVietChiaSe,
+      this.idLoai,
+      this.hinhAnh,
+      this.trangThai);
+}
